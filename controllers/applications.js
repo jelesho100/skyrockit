3782915e7fdedf6519a57c9e1cf3f656a56c7e4a
jelesho <jelesho100@gmail.com>
2025-07-24 15:27:02 -0400

    create show functionality

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,7 +6,7 @@ const User = require('../models/user.js');
 router.get('/', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id);
-        
+
         res.render('applications/index.ejs', {
             applications: currentUser.applications,
         });
@@ -24,17 +24,30 @@ router.get('/new', async (req, res) => {
 //POST for '/applications
 router.post('/', async (req, res) => {
     try {
-        // Look up the user from req.session
-        const currentUser = await User.findById(req.session.user._id);
-        // Push req.body (the new form data object) to the
-        // applications array of the current user
+        const currentUser = await User.findById(req.session.user._id);         // Look up the user from req.session
+        // Push req.body (the new form data object) to the // applications array of the current user
         currentUser.applications.push(req.body);
         // Save changes to the user
         await currentUser.save();
         // Redirect back to the applications index view
         res.redirect(`/users/${currentUser._id}/applications`);
     } catch (error) {
-        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+//get to /applications/:applicationId
+router.get('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);         // Look up the user from req.session
+        // Find the application by the applicationId supplied from req.params
+        const application = currentUser.applications.id(req.params.applicationId);
+        // Render the show view, passing the application data in the context object
+        res.render('applications/show.ejs', {
+            application: application,
+        });
+    } catch (error) {         // If any errors, log them and redirect back home
         console.log(error);
         res.redirect('/');
     }
